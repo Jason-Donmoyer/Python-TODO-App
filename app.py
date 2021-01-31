05ad2a6ece8b661359396d2e3275658ba93f0e4f
Jason-Donmoyer <j.donmoyer523@gmail.com>
2021-01-31 07:14:18 -0500

    html form, flask route to handle create todos

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
-from flask import Flask, render_template
+# IMPORTS
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jasondonmoyer@localhost:5432/tasks'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Task: {self.id} objective: {description}'
+
+
+db.create_all()
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'Todo1'
-    }, {
-        'description': 'Todo2'
-    }, {
-        'description': 'Todo3'
-    }])
+    return render_template('index.html', data=Todo.query.all())
 
 
 if __name__ == '__main__':
