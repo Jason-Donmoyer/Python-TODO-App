4f65a2f85e227ac800dceb81b4f744b7c1612014
Jason-Donmoyer <j.donmoyer523@gmail.com>
2021-02-01 08:15:06 -0500

    converted post request to fetch function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # IMPORTS
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -24,11 +24,13 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.form.get('description', '')
+    description = request.get_json()['description']
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return jsonify({
+        'description': todo.description
+    })
 
 
 @app.route('/')
